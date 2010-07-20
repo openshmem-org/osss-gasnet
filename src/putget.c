0c83d2c0f49ad71a4fbec95eff30146c759c7441
Tony Curtis <arcurtis@mail.uh.edu>
2010-07-20 01:31:54 +0000

    Wrapped single put/get routines for larger data types

--- src/putget.c
+++ src/putget.c
@@ -9,6 +9,28 @@
 #include "stats.h"
 #include "updown.h"
 
+
+#define SHMEM_TYPE_PUT_NBI(Name, Type)					\
+  void									\
+  __shmem_##Name##_put_nbi(Type *target, Type *source, size_t len, int pe) \
+  {									\
+    gasnet_put_nbi(pe, target, source, sizeof(Type) * len);		\
+  }
+
+SHMEM_TYPE_PUT_NBI(short, short)
+SHMEM_TYPE_PUT_NBI(int, int)
+SHMEM_TYPE_PUT_NBI(long, long)
+SHMEM_TYPE_PUT_NBI(longdouble, long double)
+SHMEM_TYPE_PUT_NBI(longlong, long long)
+SHMEM_TYPE_PUT_NBI(double, double)
+SHMEM_TYPE_PUT_NBI(float, float)
+
+void
+__shmem_wait_syncnbi_puts(void)
+{
+  gasnet_wait_syncnbi_puts();
+}
+
 /*
  * short-circuit local puts/gets, otherwise translate between
  * local/remote addresses
@@ -19,7 +41,7 @@
  * TODO: provide address translation routine (note repeated offset code below)
  */
 
-#define SHMEM_TYPE_PUT_EMIT(Name, Type)					\
+#define SHMEM_TYPE_PUT(Name, Type)					\
   void									\
   shmem_##Name##_put(Type *dest, const Type *src, size_t len, int pe)	\
   {									\
@@ -44,13 +66,13 @@
     SHMEM_STATS_PUT(pe);						\
   }
 
-SHMEM_TYPE_PUT_EMIT(short, short)
-SHMEM_TYPE_PUT_EMIT(int, int)
-SHMEM_TYPE_PUT_EMIT(long, long)
-SHMEM_TYPE_PUT_EMIT(longlong, long long)
-SHMEM_TYPE_PUT_EMIT(longdouble, long double)
-SHMEM_TYPE_PUT_EMIT(double, double)
-SHMEM_TYPE_PUT_EMIT(float, float)
+SHMEM_TYPE_PUT(short, short)
+SHMEM_TYPE_PUT(int, int)
+SHMEM_TYPE_PUT(long, long)
+SHMEM_TYPE_PUT(longlong, long long)
+SHMEM_TYPE_PUT(longdouble, long double)
+SHMEM_TYPE_PUT(double, double)
+SHMEM_TYPE_PUT(float, float)
 
 _Pragma("weak shmem_putmem=shmem_long_put")
 _Pragma("weak shmem_put=shmem_long_put")
@@ -59,7 +81,7 @@ _Pragma("weak shmem_put64=shmem_long_put")
 _Pragma("weak shmem_put128=shmem_longdouble_put")
 
   
-#define SHMEM_TYPE_GET_EMIT(Name, Type)					\
+#define SHMEM_TYPE_GET(Name, Type)					\
   void									\
   shmem_##Name##_get(Type *dest, const Type *src, size_t len, int pe) \
   {									\
@@ -84,13 +106,13 @@ _Pragma("weak shmem_put128=shmem_longdouble_put")
     SHMEM_STATS_GET(pe);						\
   }
 
-SHMEM_TYPE_GET_EMIT(short, short)
-SHMEM_TYPE_GET_EMIT(int, int)
-SHMEM_TYPE_GET_EMIT(long, long)
-SHMEM_TYPE_GET_EMIT(longdouble, long double)
-SHMEM_TYPE_GET_EMIT(longlong, long long)
-SHMEM_TYPE_GET_EMIT(double, double)
-SHMEM_TYPE_GET_EMIT(float, float)
+SHMEM_TYPE_GET(short, short)
+SHMEM_TYPE_GET(int, int)
+SHMEM_TYPE_GET(long, long)
+SHMEM_TYPE_GET(longdouble, long double)
+SHMEM_TYPE_GET(longlong, long long)
+SHMEM_TYPE_GET(double, double)
+SHMEM_TYPE_GET(float, float)
 
 _Pragma("weak shmem_getmem=shmem_long_get")
 _Pragma("weak shmem_get=shmem_long_get")
@@ -98,27 +120,21 @@ _Pragma("weak shmem_get32=shmem_int_get")
 _Pragma("weak shmem_get64=shmem_long_get")
 _Pragma("weak shmem_get128=shmem_longdouble_get")
 
-#define SHMEM_TYPE_PUT_NBI(Name, Type)					\
-  void									\
-  __shmem_##Name##_put_nbi(Type *target, Type *source, size_t len, int pe) \
+/*
+ * gasnet_(put|get)_val can't handle bigger types..
+ */
+
+#define SHMEM_TYPE_P_WRAPPER(Name, Type)				\
+  __inline__ void							\
+  shmem_##Name##_p(Type *dest, Type value, int pe)			\
   {									\
-    gasnet_put_nbi(pe, target, source, sizeof(Type) * len);		\
+    shmem_##Name##_put(dest, &value, sizeof(value), pe);		\
   }
 
-SHMEM_TYPE_PUT_NBI(short, short)
-SHMEM_TYPE_PUT_NBI(int, int)
-SHMEM_TYPE_PUT_NBI(long, long)
-SHMEM_TYPE_PUT_NBI(longdouble, long double)
-SHMEM_TYPE_PUT_NBI(longlong, long long)
-SHMEM_TYPE_PUT_NBI(double, double)
-SHMEM_TYPE_PUT_NBI(float, float)
-
-void
-__shmem_wait_syncnbi_puts(void)
-{
-  gasnet_wait_syncnbi_puts();
-}
-
+SHMEM_TYPE_P_WRAPPER(float, float)
+SHMEM_TYPE_P_WRAPPER(double, double)
+SHMEM_TYPE_P_WRAPPER(longdouble, long double)
+SHMEM_TYPE_P_WRAPPER(longlong, long long)
 
 #define SHMEM_TYPE_P(Name, Type)					\
   void									\
@@ -149,12 +165,19 @@ SHMEM_TYPE_P(char, char)
 SHMEM_TYPE_P(short, short)
 SHMEM_TYPE_P(int, int)
 SHMEM_TYPE_P(long, long)
-SHMEM_TYPE_P(longlong, long long)
-SHMEM_TYPE_P(float, float)
-SHMEM_TYPE_P(double, double)
-SHMEM_TYPE_P(longdouble, long double)
 
-  
+#define SHMEM_TYPE_G_WRAPPER(Name, Type)				\
+  __inline__ void							\
+  shmem_##Name##_g(Type *dest, Type value, int pe)			\
+  {									\
+    shmem_##Name##_get(dest, &value, sizeof(value), pe);		\
+  }
+
+SHMEM_TYPE_G_WRAPPER(float, float)
+SHMEM_TYPE_G_WRAPPER(double, double)
+SHMEM_TYPE_G_WRAPPER(longlong, long long)
+SHMEM_TYPE_G_WRAPPER(longdouble, long double)
+
 #define SHMEM_TYPE_G(Name, Type)					\
   Type									\
   shmem_##Name##_g(Type *src, int pe)					\
@@ -176,7 +199,7 @@ SHMEM_TYPE_P(longdouble, long double)
 		     );							\
 	__shmem_exit(1);						\
       }									\
-      retval = gasnet_get_val(pe, their_src, typed_len);		\
+      retval = (Type) gasnet_get_val(pe, their_src, typed_len);		\
     }									\
     SHMEM_STATS_GET(pe);						\
     return retval;							\
@@ -187,7 +210,3 @@ SHMEM_TYPE_G(char, char)
 SHMEM_TYPE_G(short, short)
 SHMEM_TYPE_G(int, int)
 SHMEM_TYPE_G(long, long)
-SHMEM_TYPE_G(longlong, long long)
-SHMEM_TYPE_G(float, float)
-SHMEM_TYPE_G(double, double)
-SHMEM_TYPE_G(longdouble, long double)
