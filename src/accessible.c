2274beb6d5779481d8d8bb8dbb314ae8a8c2774b
Tony Curtis <arcurtis@mail.uh.edu>
2010-09-20 20:26:27 +0000

    Note future work on PE reachability

--- src/accessible.c
+++ src/accessible.c
@@ -2,6 +2,9 @@
 
 /*
  * assume yes, for now
+ *
+ * TODO: what kind of tests could we do to really check this?
+ *       (maybe send a test ping-pong message)
  */
 int
 shmem_pe_accessible(int pe)
