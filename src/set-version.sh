68767dfb718b8781a6ba499e51216c07180477b2
Tony Curtis <arcurtis@mail.uh.edu>
2010-12-02 19:10:47 +0000

    Forgot to SVN'ify the version creator

---
+++ src/set-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rev="`svn info | grep '^Revision:'`"
+
+cat >version.c <<EOT
+char *
+shmem_version(void)
+{
+  return "Super Happy Fun OpenSHMEM, $rev";
+}
+
+#ifdef HAVE_PSHMEM_SUPPORT
+_Pragma("weak pshmem_version=shmem_version")
+#endif /* HAVE_PSHMEM_SUPPORT */
+EOT
