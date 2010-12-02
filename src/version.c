a2b7c9ceb10a1c6c8feef137a73cabb0370f362d
Tony Curtis <arcurtis@mail.uh.edu>
2010-12-02 19:09:30 +0000

    Making sure we get the newest version out of SVN

--- src/version.c
+++ src/version.c
@@ -1,9 +1,8 @@
 char *
 shmem_version(void)
 {
-  return "Super Happy Fun OpenSHMEM $Rev$";
+  return "Super Happy Fun OpenSHMEM, Revision: 1233";
 }
-
 
 #ifdef HAVE_PSHMEM_SUPPORT
 _Pragma("weak pshmem_version=shmem_version")
