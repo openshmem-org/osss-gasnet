23d350301d553a9ebe11a019d7bb74d06518a515
Tony Curtis <arcurtis@mail.uh.edu>
2010-12-10 19:46:40 +0000

    Add a few more pshmem interfaces

--- src/query.c
+++ src/query.c
@@ -32,3 +32,15 @@ shmem_nodename(void)
 {
   return __state.loc.nodename;
 }
+
+
+#ifdef HAVE_PSHMEM_SUPPORT
+_Pragma("weak pshmem_my_pe=shmem_my_pe")
+_Pragma("weak pmy_pe=my_pe")
+_Pragma("weak p_my_pe=_my_pe")
+
+_Pragma("weak pshmem_num_pes=shmem_num_pes")
+_Pragma("weak pshmem_n_pes=shmem_n_pes")
+_Pragma("weak pnum_pes=num_pes")
+_Pragma("weak p_num_pes=_num_pes")
+#endif /* HAVE_PSHMEM_SUPPORT */
