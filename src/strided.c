f07c666c0d5229f46a32037e0de03d7c1cd3467f
Tony Curtis <arcurtis@mail.uh.edu>
2010-12-14 20:27:24 +0000

    Rest of strided get code

--- src/strided.c
+++ src/strided.c
@@ -49,3 +49,54 @@ _Pragma("weak pshmem_iput32=shmem_iput32")
 _Pragma("weak pshmem_iput64=shmem_iput64")
 _Pragma("weak pshmem_iput128=shmem_iput128")
 #endif /* HAVE_PSHMEM_SUPPORT */
+
+
+
+
+/*
+ *
+ * void shmem_int_iget(int *target, symmetric const int *source,
+ *                     ptrdiff_t target_stride,
+ *	   	       ptrdiff_t source_stride,
+ *                     size_t n_elems, int pe);
+ *
+ */
+
+#define SHMEM_EMIT_IGET(Name, Type)					\
+  /* @api@ */								\
+  void									\
+  shmem_##Name##_iget(Type *target, const Type *source,			\
+		      ptrdiff_t tst, ptrdiff_t sst, size_t nelems, int pe) \
+  {									\
+    size_t ti = 0, si = 0;						\
+    size_t i;								\
+    for (i = 0; i < nelems; i += 1) {					\
+      target[ti] = shmem_##Name##_g((Type *) & (source[si]), pe);	\
+      ti += tst;							\
+      si += sst;							\
+    }									\
+  }
+
+SHMEM_EMIT_IGET(short, short)
+SHMEM_EMIT_IGET(int, int)
+SHMEM_EMIT_IGET(long, long)
+SHMEM_EMIT_IGET(double, double)
+SHMEM_EMIT_IGET(float, float)
+SHMEM_EMIT_IGET(longdouble, long double)
+SHMEM_EMIT_IGET(longlong, long long)
+
+_Pragma("weak shmem_iget32=shmem_int_iget")
+_Pragma("weak shmem_iget64=shmem_long_iget")
+_Pragma("weak shmem_iget128=shmem_longdouble_iget")
+
+#ifdef HAVE_PSHMEM_SUPPORT
+_Pragma("weak pshmem_short_iget=shmem_short_iget")
+_Pragma("weak pshmem_int_iget=shmem_int_iget")
+_Pragma("weak pshmem_long_iget=shmem_long_iget")
+_Pragma("weak pshmem_double_iget=shmem_double_iget")
+_Pragma("weak pshmem_longdouble_iget=shmem_longdouble_iget")
+_Pragma("weak pshmem_longlong_iget=shmem_longlong_iget")
+_Pragma("weak pshmem_iget32=shmem_iget32")
+_Pragma("weak pshmem_iget64=shmem_iget64")
+_Pragma("weak pshmem_iget128=shmem_iget128")
+#endif /* HAVE_PSHMEM_SUPPORT */
