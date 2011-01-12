93091c7b8f4247b60aaa4ce785550b18e7d02e4c
Tony Curtis <arcurtis@mail.uh.edu>
2011-01-12 18:30:18 +0000

    Accidentally included automatically-generated files

--- src/updown.h
+++ src/updown.h
@@ -1,6 +1,11 @@
 #ifndef _UPDOWN_H
 #define _UPDOWN_H 1
 
+/*
+ * trigger the shutdown process (with exit status)
+ *
+ */
+
 extern void __shmem_exit(int status);
 
 #endif /* _UPDOWN_H */
--- src/version.c
+++ src/version.c
@@ -5,7 +5,7 @@
 char *
 shmem_version(void)
 {
-  return "Super Happy Fun OpenSHMEM, Revision: 1395";
+  return "Super Happy Fun OpenSHMEM, Revision: 1397";
 }
 
 #ifdef HAVE_PSHMEM_SUPPORT
