d756cde98f876405db06d3b8c5787f7311bd4be7
Tony Curtis <arcurtis@mail.uh.edu>
2011-02-07 22:16:04 +0000

    Another old example fixup

--- examples/C/bcast2.c
+++ examples/C/bcast2.c
@@ -2,11 +2,11 @@
 
 #include <mpp/shmem.h>
 
+long pSync[_SHMEM_BCAST_SYNC_SIZE];
+
 int
 main(void)
 {
-  int i;
-  long *pSync;
   long *target;
   long source[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
   int nlong = 8;
@@ -17,24 +17,20 @@ main(void)
 
   target = (long *) shmalloc( 8 * sizeof(*target) );
 
-  pSync = (long *) shmalloc( _SHMEM_BCAST_SYNC_SIZE * sizeof(*pSync) );
+  shmem_sync_init(pSync);
 
-  for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i++) {
-    pSync[i] = _SHMEM_SYNC_VALUE;
-  }
-  shmem_barrier_all();
+  shmem_broadcast64(target, source, nlong, 0, 0, 0, 4, pSync);
 
-  shmem_broadcast64(target, source, nlong, 0, 4, 1, 4, pSync);
-
-  for (i = 0; i < 8; i++) {
-    printf("%d: target[%d] = %ld\n", me, i, target[i]);
+  if (me != 0) {
+    int i;
+    for (i = 0; i < 8; i++) {
+      printf("%d: target[%d] = %ld\n", me, i, target[i]);
+    }
   }
   
   shmem_barrier_all();
 
-  shfree(pSync);
   shfree(target);
 
   return 0;
 }
-
