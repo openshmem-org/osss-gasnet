27f4b5c807c03e5d6ad54604eb3965f1e0db16e5
Tony Curtis <arcurtis@mail.uh.edu>
2011-02-11 22:37:41 +0000

    Fix some problems in old example

--- examples/C/fcollect.c
+++ examples/C/fcollect.c
@@ -1,53 +1,56 @@
+/*
+ * run on 4 PEs
+ *
+ */
+
 #include <stdio.h>
 #include <string.h>
 
-#include <assert.h>
+#include <mpp/shmem.h>
 
-#include <shmem.h>
+long pSync[_SHMEM_BCAST_SYNC_SIZE];
 
 int
 main(void)
 {
   int npes;
   int me;
-  int *dst, *src;
+  int *dst;
+  static int src;
   int i;
-  long *pSync;
 
-  shmem_init();
-  npes = shmem_num_pes();
-  me = shmem_my_pe();
+  start_pes(0);
+  npes = _num_pes();
+  me = _my_pe();
 
   dst = shmalloc(sizeof(*dst) * 4);
-  src = shmalloc(sizeof(*src));
 
   for (i = 0; i < 4; i++) {
     dst[i] = 10101;
   }
-  *src = me + 100;
+  src = me + 100;
 
   shmem_barrier_all();
 
-  pSync = shmem_sync_init();
+  shmem_sync_init(pSync);
 
   printf("%8s: dst[%d/%d] = %d, %d, %d, %d\n",
          "BEFORE", me, npes, dst[0], dst[1], dst[2], dst[3]);
 
-  shmem_fcollect32(dst, src, 1,
-                   0, 1, 4,
+  shmem_fcollect32(dst, &src, 1,
+                   0, 0, npes,
                    pSync);
 
   shmem_barrier_all();
 
   /*
-   * state of "dst", indices 0, 2, 4, 6 should be "src" values
+   * end state of "dst" = 100, 101, 102, ...
    */
 
   printf("%8s: dst[%d/%d] = %d, %d, %d, %d\n",
          "AFTER", me, npes, dst[0], dst[1], dst[2], dst[3]);
 
   shfree(dst);
-  shfree(src);
 
   return 0;
 }
