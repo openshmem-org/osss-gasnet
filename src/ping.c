c3b9d6db0f1a210a7851be640ed1c0910efc33a1
Tony Curtis <arcurtis@mail.uh.edu>
2011-02-12 05:40:36 +0000

    Allow runtime setting of accessibility timeout

--- src/ping.c
+++ src/ping.c
@@ -6,23 +6,26 @@ static const char *ping_timeout_envvar = "SHMEM_PE_ACCESSIBLE_TIMEOUT";
 static const int DEFAULT_PE_ACCESSIBLE_TIMEOUT = 1;
 
 void
+__shmem_set_ping_timeout(int secs)
+{
+  __state.ping_timeout = secs;
+}
+
+void
 __shmem_ping_init(void)
 {
-  int timeout;
+  int timeout = DEFAULT_PE_ACCESSIBLE_TIMEOUT;;
   char *pt = __comms_getenv(ping_timeout_envvar);
 
   if (pt != (char *) NULL) {
     timeout = atoi(pt);
+    /* sanity check */
+    if (timeout < 0) {
+      timeout = DEFAULT_PE_ACCESSIBLE_TIMEOUT;
+    }      
   }
-  else {
-    timeout = DEFAULT_PE_ACCESSIBLE_TIMEOUT;
-  }
-  /* sanity check */
-  if (timeout < 0) {
-    timeout = DEFAULT_PE_ACCESSIBLE_TIMEOUT;
-  }      
 
-  __state.ping_timeout = timeout;
+  __shmem_set_ping_timeout(timeout);
 
   __shmem_trace(SHMEM_LOG_INIT,
 		"PE accessibility timeout set to %d sec",
--- src/ping.h
+++ src/ping.h
@@ -1,6 +1,8 @@
 #ifndef _PING_H
 #define _PING_H 1
 
+extern void __shmem_set_ping_timeout(int secs);
+
 extern void __shmem_ping_init(void);
 
 #endif /* _PING_H */
