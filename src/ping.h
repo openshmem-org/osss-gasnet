cd0e281b7c4dabc117809174b59559d413ca6b67
Tony Curtis <arcurtis@mail.uh.edu>
2011-03-03 04:07:05 +0000

    Fix prototype

--- src/ping.h
+++ src/ping.h
@@ -1,10 +1,19 @@
 #ifndef _PING_H
 #define _PING_H 1
 
-extern void __shmem_set_ping_timeout(int secs);
-
+/*
+ * initialize the ping subsystem
+ */
 extern void __shmem_ping_init(void);
 
+/*
+ * set the amount of time to wait
+ */
+extern void __shmem_set_ping_timeout(double secs);
+
+/*
+ * set & clear alarms
+ */
 extern void __shmem_ping_set_alarm(void);
 extern void __shmem_ping_clear_alarm(void);
 
