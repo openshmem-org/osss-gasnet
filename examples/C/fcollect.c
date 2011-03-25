98698b3598a4fc53b3763d32713363baac145fee
Tony Curtis <arcurtis@mail.uh.edu>
2011-03-25 20:35:05 +0000

    Move the "before" print statement ahead of the barrier

--- examples/C/fcollect.c
+++ examples/C/fcollect.c
@@ -30,13 +30,13 @@ main(void)
   }
   src = me + 100;
 
+  printf("%8s: dst[%d/%d] = %d, %d, %d, %d\n",
+         "BEFORE", me, npes, dst[0], dst[1], dst[2], dst[3]);
+
   for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1) {
     pSync[i] = _SHMEM_SYNC_VALUE;
   }
   shmem_barrier_all();
-
-  printf("%8s: dst[%d/%d] = %d, %d, %d, %d\n",
-         "BEFORE", me, npes, dst[0], dst[1], dst[2], dst[3]);
 
   shmem_fcollect32(dst, &src, 1,
                    0, 0, npes,
