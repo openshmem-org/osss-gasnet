6948a7ad7d6807c9d423e5f17f3422c847979b4d
Tony Curtis <arcurtis@mail.uh.edu>
2011-04-13 01:31:27 +0000

    Header fix

--- src/globalvar/globalvar.h
+++ src/globalvar/globalvar.h
@@ -5,6 +5,8 @@
  * memory classification and accessibility
  */
 
+extern void   __shmem_symmetric_globalvar_table_init(void);
+
 extern int    __shmem_symmetric_is_globalvar(void *addr);
 
 extern void   __shmem_symmetric_memory_init(void);
