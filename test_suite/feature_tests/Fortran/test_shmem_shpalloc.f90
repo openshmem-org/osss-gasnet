21c0b631a61500edeec0a92e469a077ce22eae39
Ricardo Mauricio <remauric@mail.uh.edu>
2011-09-29 14:29:57 -0500

    Fixed shpalloc test to correctly use cray pointers.

--- test_suite/feature_tests/Fortran/test_shmem_shpalloc.f90
+++ test_suite/feature_tests/Fortran/test_shmem_shpalloc.f90
@@ -1,3 +1,5 @@
+! Attempts to allocate an array in the symmetric heap.
+! The test passes if we can correctly PUT data into that object on a remote PE.
 program test_shmem_shpalloc
   implicit none
   include 'mpp/shmem.fh'
@@ -5,9 +7,8 @@ program test_shmem_shpalloc
   integer, parameter :: N = 3
 
   integer*8          :: ptr
-  integer            :: dst(*)
+  integer            :: dst(1)    
   pointer            (ptr, dst)
-  integer            :: dst
   integer            :: src(N)
   integer            :: success ! this should probably be renamed to failed :)
 
@@ -16,27 +17,28 @@ program test_shmem_shpalloc
 ! Function definitons
   integer    :: my_pe, num_pes
 
+  call start_pes(0)
+
   me  = my_pe()
   npes = num_pes()
 
   call shpalloc(ptr, N, errcode, abort)
+
   do i = 1, N, 1
     dst(i) = -9
     src(i) = me
   end do
 
-
   call shmem_barrier_all()
 
   if(me .eq. 1) then
-    !call shmem_integer_put(dst, src, N, 0)
     call shmem_integer_put(dst, src, N, 0)
   end if
 
   call shmem_barrier_all()
 
   success = 0
-  if(me .eq. 0) then
+  if(me .eq. 0) then  	
     do i = 1, N, 1
       if(dst(i) .ne. 1) then
         success = 1
@@ -44,14 +46,18 @@ program test_shmem_shpalloc
     end do
   end if
 
-  if(success .eq. 0) then
-    write (*,*) "Test shpalloc(): Passed"
-  else
-    write (*,*) "Test shpalloc(): Failed"
-  end if
-
+	if(me .eq. 0) then
+	  if(success .eq. 0) then
+	    write (*,*) "Test shpalloc(): Passed"
+	  else
+	    write (*,*) "Test shpalloc(): Failed"
+	  end if
+	end if
+	
   call shmem_barrier_all()
 
-  call shpdeallc(ptr)
+  call shpdeallc(ptr, errcode, abort)
+  
+  ! TODO: test if shpdeallc really deallocates memory
 
 end program
