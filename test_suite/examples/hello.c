5b596b0c7b828ede9e39e137da29600dd7352d96
Swaroop Pophale <swaroop@shark.pstl.uh.edu>
2011-10-22 01:23:23 -0500

    Adding hello.c

---
+++ test_suite/examples/hello.c
@@ -0,0 +1,21 @@
+/* (c) 2011 University of Houston System.  All rights reserved. */
+
+
+#include <stdio.h>
+
+#include <mpp/shmem.h>
+
+int
+main(int argc, char **argv)
+{
+  int me, npes;
+
+  start_pes(0);
+
+  me   = _my_pe();
+  npes = _num_pes();
+
+  printf("Hello from node %4d of %4d\n", me, npes); 
+
+  return 0;
+}
