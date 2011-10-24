006a4e170cd29f684ee566051127b7a6d5e5fa63
Swaroop Pophale <swaroop@crill-002.pstl.uh.edu>
2011-10-23 23:33:03 -0500

    Edit

--- test_suite/feature_tests/C/test_shmem_accessible.c
+++ test_suite/feature_tests/C/test_shmem_accessible.c
@@ -21,7 +21,7 @@ main(int argc, char *argv[])
 {
   long local_target;
   int *shm_target;
-  char *msg = "Test Address Accessable: Passed";
+  char *msg = "Test Address Accessible: Passed";
   int me,npes,i;
   int pe_acc_success=0;
 
@@ -36,30 +36,30 @@ main(int argc, char *argv[])
   if (me == 0) {
 
     if (! check_it(&global_target)) { /* long global: yes */
-      printf("Test Global Address Accessable: Failed\n");
+      printf("Test Global Address Accessible: Failed\n");
     }
     else{
-      printf("Test Global Address Accessable: Passed\n");  
+      printf("Test Global Address Accessible: Passed\n");  
     }
     if (! check_it(&static_target)) { /* static int global: yes */
-      printf("Test Static Global Address Accessable: Failed\n");
+      printf("Test Static Global Address Accessible: Failed\n");
     }
     else{
-      printf("Test Static Global Address Accessable: Passed\n");  
+      printf("Test Static Global Address Accessible: Passed\n");  
     }
     if (check_it(&local_target)) { /* main() stack: no  */
-      printf("Test Stack Address Accessable: Failed\n");
+      printf("Test Stack Address Accessible: Failed\n");
 
     }
     else{
-      printf("Test Stack Address Accessable: Passed\n");  
+      printf("Test Stack Address Accessible: Passed\n");  
     }
     if (! check_it(shm_target)) { /* shmalloc: yes */
 
-      printf("Test Shmalloc-ed Address Accessable: Failed\n");
+      printf("Test Shmalloc-ed Address Accessible: Failed\n");
     }
     else{
-      printf("Test Shmalloc-ed Address Accessable: Passed\n");  
+      printf("Test Shmalloc-ed Address Accessible: Passed\n");  
     }
 
 
