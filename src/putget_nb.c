a439c1ae1a3fd1470fda34c7acf9e72dcbdac6f6
Tony Curtis <arcurtis@mail.uh.edu>
2011-11-02 12:27:03 -0500

    adding some spacing

--- src/putget_nb.c
+++ src/putget_nb.c
@@ -67,6 +67,7 @@ SHMEM_TYPE_PUT_NB (longdouble, long double)
 SHMEM_TYPE_PUT_NB (longlong, long long)
 SHMEM_TYPE_PUT_NB (double, double)
 SHMEM_TYPE_PUT_NB (float, float)
+
 #pragma weak shmem_short_put_nb = pshmem_short_put_nb
 #pragma weak shmem_int_put_nb = pshmem_int_put_nb
 #pragma weak shmem_long_put_nb = pshmem_long_put_nb
@@ -74,6 +75,7 @@ SHMEM_TYPE_PUT_NB (float, float)
 #pragma weak shmem_longlong_put_nb = pshmem_longlong_put_nb
 #pragma weak shmem_double_put_nb = pshmem_double_put_nb
 #pragma weak shmem_float_put_nb = pshmem_float_put_nb
+
 #define SHMEM_TYPE_GET_NB(Name, Type)					\
   /* @api@ */								\
   void *								\
@@ -83,6 +85,7 @@ SHMEM_TYPE_PUT_NB (float, float)
     PE_RANGE_CHECK(pe);							\
     return __shmem_comms_##Name##_get_nb(target, source, nelems, pe);	\
   }
+
 SHMEM_TYPE_GET_NB (short, short)
 SHMEM_TYPE_GET_NB (int, int)
 SHMEM_TYPE_GET_NB (long, long)
@@ -90,6 +93,7 @@ SHMEM_TYPE_GET_NB (longdouble, long double)
 SHMEM_TYPE_GET_NB (longlong, long long)
 SHMEM_TYPE_GET_NB (double, double)
 SHMEM_TYPE_GET_NB (float, float)
+
 #pragma weak shmem_short_get_nb = pshmem_short_get_nb
 #pragma weak shmem_int_get_nb = pshmem_int_get_nb
 #pragma weak shmem_long_get_nb = pshmem_long_get_nb
@@ -97,8 +101,9 @@ SHMEM_TYPE_GET_NB (float, float)
 #pragma weak shmem_longlong_get_nb = pshmem_longlong_get_nb
 #pragma weak shmem_double_get_nb = pshmem_double_get_nb
 #pragma weak shmem_float_get_nb = pshmem_float_get_nb
+
 /* @api@ */
-     void pshmem_wait_nb (void *h)
+void pshmem_wait_nb (void *h)
 {
   __shmem_comms_wait_nb (h);
 }
