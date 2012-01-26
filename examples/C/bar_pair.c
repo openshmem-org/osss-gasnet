caecc9957683097bedcf4a826c1e0a7ce0eb9fd5
Ramachandra CN <ram@shark.pstl.uh.edu>
2012-01-25 18:06:58 -0600

    Added new examples showing usage of some api's

---
+++ examples/C/bar_pair.c
@@ -0,0 +1,83 @@
+/*
+ *
+ * Copyright (c) 2011, University of Houston System and Oak Ridge National
+ * Laboratory.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * o Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * 
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 
+ * o Neither the name of the University of Houston System, Oak Ridge
+ *   National Laboratory nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* Program to illustrate the usage of shmem_barrier between a pair of PE's*/
+
+#include <stdio.h>
+#include <mpp/shmem.h>
+
+#define NPES 4
+
+long pSync[_SHMEM_BCAST_SYNC_SIZE];
+int x = 10101;
+
+int
+main()
+{
+  int me, npes;
+  int i;
+
+  for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1) {
+    pSync[i] = _SHMEM_SYNC_VALUE;
+  }
+
+  start_pes(0);
+  me = _my_pe();
+  npes = _num_pes();
+
+  shmem_barrier_all();
+
+  if (me == 0) {
+    shmem_int_p(&x, 4, 1);
+  }
+  
+  if(me==2) {
+    printf("Process %d going to sleep\n",me);
+    sleep(3);
+    printf("Process %d out from sleep\n",me);
+  }
+
+  printf("Process %d before barrier\n",me);
+  if(me == 2 || me == 3) {
+    shmem_barrier(2, 0, 2,  pSync);
+  }
+  printf("Process %d after barrier\n",me);
+
+  printf("%d: x = %d\n", me, x);
+
+  return 0;
+}
---
+++ examples/C/shmem_twosided.c
@@ -0,0 +1,98 @@
+/*
+ *
+ * Copyright (c) 2011, University of Houston System and Oak Ridge National
+ * Laboratory.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * o Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * 
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 
+ * o Neither the name of the University of Houston System, Oak Ridge
+ *   National Laboratory nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * This program shows, one to one communication and synchronization 
+ */
+
+//#include "mpi.h"
+#include <mpp/shmem.h>
+#include <stdio.h>
+int numtasks, rank;
+
+// make the recieve argument global, shmem accessible
+int inmsg1=9999;
+int inmsg2=9999;
+
+void even() {
+  int outmsg1=4;
+  int dest = 1;
+  int source = 1;
+  int rc, count, tag=1;  
+  //MPI_Status Stat;
+  //rc = MPI_Send(&outmsg1, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+  //rc = MPI_Recv(&inmsg1, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+  shmem_int_put(&inmsg2,&outmsg1,1,dest);
+  if(inmsg1==9999)
+    shmem_int_wait(&inmsg1,9999);
+  printf("Task %d: Received %d\n",rank,inmsg1);
+}
+
+void odd() {
+  int dest = 0;
+  int source = 0;
+  int outmsg2=7;
+  int rc, count, tag=1;  
+  int temp2;
+  //MPI_Status Stat;
+  //rc = MPI_Recv(&inmsg2, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+  //rc = MPI_Send(&outmsg2, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+  shmem_int_put(&inmsg1,&outmsg2,1,dest);
+  if(inmsg2 == 9999)
+    shmem_int_wait(&inmsg2,9999);
+  printf("Task %d: Received %d\n",rank,inmsg2);
+}
+
+int main( int argc, char *argv[])  {
+
+  //MPI_Init(&argc,&argv);
+  //MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  start_pes(0);
+  rank = _my_pe();
+  numtasks = _num_pes();
+
+  if (rank == 0) {
+    even();
+  } 
+  else if (rank == 1) {
+    odd();
+  }
+
+  //MPI_Finalize();
+}
