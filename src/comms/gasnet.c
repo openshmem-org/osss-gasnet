16abf85d468f5b7e0911323be2194cd70771dddd
Tony Curtis <arcurtis@mail.uh.edu>
2012-03-03 10:27:28 -0600

    Oops, left GASNet timeout experiment in wrong place...

--- src/comms/gasnet.c
+++ src/comms/gasnet.c
@@ -607,11 +607,11 @@ __shmem_comms_barrier_all (void)
 {
   /* GASNET_BEGIN_FUNCTION(); */
 
-  __shmem_service_reset ();
-
   /* use gasnet's global barrier */
   gasnet_barrier_notify (barcount, barflag);
   GASNET_SAFE (gasnet_barrier_wait (barcount, barflag));
+
+  __shmem_service_reset ();
 
   /* barcount = 1 - barcount; */
   barcount += 1;
