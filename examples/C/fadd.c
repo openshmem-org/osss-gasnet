787c6141fcd8cea37f155a50b34dbfba9422bb0a
Tony Curtis <tonyc@uh.edu>
2013-09-06 21:20:01 -0500

    Fix doc typo in example
    Add couple READMEs

--- examples/C/fadd.c
+++ examples/C/fadd.c
@@ -38,10 +38,10 @@
 
 
 /*
- * expected output on 2 PEs:
+ * expected output on 2 PEs (lines can appear in any order):
  *
- * 0: old = 22, dst = 22
- * 1: old = -1, dst = 66
+ * 1: old = 22, dst = 22
+ * 0: old = -1, dst = 66
  *
  */
 
